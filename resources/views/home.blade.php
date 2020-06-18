ec1a084ba915a0ce025dd896831e2a2c34bc7f06
Alexandr Stecuk <sandrostecuk@gmail.com>
2020-06-18 18:23:51 +0300

    update(*): html, style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
     <div class="wrapper">
-        <header class="header"></header>
-        <div class="content">dsf</div>
-        <footer></footer>
+        <header class="header">
+            <div class="line"></div>
+            <nav class="menu">
+
+            </nav>
+            <section class="slider">
+                <img id="sliderImage1" class="slider-image" src="/images/slider-img1.png" alt="kid">
+                {{-- <img id="sliderImage2" class="slider-image" src="/images/slider-img2.png" alt="kid">
+                <img id="sliderImage3" class="slider-image" src="/images/slider-img3.png" alt="kid">
+                <img id="sliderImage4" class="slider-image" src="/images/slider-img4.png" alt="kid"> --}}
+            </section>
+        </header>
+        <div class="content">
+            <section class="model">
+                <h1 class="title"><b>Модель діяльності «GoogLiKiI»</b></h1>
+                <section class="scheme">
+                    <span class="note">ХОЧУ</span>
+                    <span class="note">МОЖУ</span>
+                    <span class="note">БУДУ</span>
+                    <span class="caption">РОЗВИТОК</span>
+                    <span class="caption">ВИХОВАННЯ</span>
+                    <span class="caption">НАВЧАННЯ</span>
+                </section>
+            </section>
+            <section class="conception"></section>
+        </div>
+        <footer>
+            <div class="line"></div>
+        </footer>
     </div>    
 </body>
 </html>
