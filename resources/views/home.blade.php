d39d8df0341793f193a867f52e451105974a18fc
Alexandr Stecuk <sandrostecuk@gmail.com>
2020-06-20 00:19:03 +0300

    update(*): style, html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/style/fontawesome/all.min.css">
+    <link rel="stylesheet" href="/style/fonts.css">
 </head>
 <body>
     <div class="wrapper">
         <header class="header">
             <div class="line"></div>
-            <nav class="menu">
-
-            </nav>
+            <nav class="menu"></nav>
             <section class="slider">
                 <img id="sliderImage1" class="slider-image" src="/images/slider-img1.png" alt="kid">
                 {{-- <img id="sliderImage2" class="slider-image" src="/images/slider-img2.png" alt="kid">
@@ -21,18 +21,65 @@
             </section>
         </header>
         <div class="content">
-            <section class="model">
+            <section class="model content-block">
                 <h1 class="title"><b>Модель діяльності «GoogLiKiI»</b></h1>
                 <section class="scheme">
-                    <span class="note">ХОЧУ</span>
-                    <span class="note">МОЖУ</span>
-                    <span class="note">БУДУ</span>
-                    <span class="caption">РОЗВИТОК</span>
-                    <span class="caption">ВИХОВАННЯ</span>
-                    <span class="caption">НАВЧАННЯ</span>
+                    <div class="cycle-block">
+                        <span class="note">ХОЧУ</span>
+                        <span class="note">МОЖУ</span>
+                        <span class="note">БУДУ</span>
+                        <img class="cycle" src="/images/cycle2.png" alt="cycle">
+                    </div>
+
+                    <div class="arrow-wrapper">
+                        <div class="arrow-block">
+                            <img class="arrow" src="/images/arrow1.png" alt="arrow">
+                            <span class="caption">РОЗВИТОК</span>
+                        </div>
+                        <div class="arrow-block">
+                            <img class="arrow" src="/images/arrow2.png" alt="arrow">
+                            <span class="caption">ВИХОВАННЯ</span>
+                        </div>
+                        <div class="arrow-block">
+                            <img class="arrow" src="/images/arrow3.png" alt="arrow">
+                            <span class="caption">НАВЧАННЯ</span>
+                        </div>
+                    </div>
+                     
+                    <span class="description">
+                        Перехід від розуміння дитиною поняття ТРЕБА до змісту  НАВІЩО ЦЕ МЕНІ ПОТРІБНО.
+                    </span>
                 </section>
             </section>
-            <section class="conception"></section>
+            <section class="line-delimiter content-block"></section>
+            <section class="conception content-block">
+                <h2 class="title-2">
+                    КОНЦЕПЦІЯ ПЕРЕДШКІЛЬНОЇ ОСВІТИ В КОЕТЕКСТІ ПІДГОТОВКИ СУЧАСНОЇ ДИТИНИ ДО ШКОЛИ
+                </h2>
+                
+                <div class="nav-block">
+                    <a href="" class="block-wrapper">
+                        <i class="fas fa-globe-americas circle"></i>
+                        <span class="link-text"> ДІЯЛЬНІСНИЙ ПІДХІД В РІХНИХ ВИДАХ АКТИВНОСТЕЙ ДИТИНИ(здобування ЗУН в активній пошуковій діяльності)</span>
+                    </a>
+                    <a href="" class="block-wrapper">
+                        <i class="fab fa-searchengin circle"></i>
+                        <span class="link-text">ДІЯЛЬНІСНИЙ ПІДХІД В РІХНИХ ВИДАХ АКТИВНОСТЕЙ ДИТИНИ(здобування ЗУН в активній пошуковій діяльності)</span>      
+                    </a>
+                    <a href="" class="block-wrapper">
+                        <i class="fas fa-thumbs-up circle"></i>
+                        <span class="link-text">ФОРМУВАННЯ БАЗОВИХ ЖИТТЄВИХ КОМПЕТЕНТНОСТЕЙ</span> 
+                    </a>
+                    <a href="" class="block-wrapper">
+                        <i class="fas fa-chart-line circle"></i>
+                        <span class="link-text">КОМФОРТНЕ ПРОЖИВАННЯ ДИТИНОЮ СПЕЦИФІЧНОЇ ДИТЯЧОЇ ДІЯЛЬНОСТІ</span>
+                    </a>
+                    <a href="" class="block-wrapper">
+                        <i class="far fa-handshake circle"></i>
+                        <span class="link-text">ПАРТНЕРСЬКА ВЗАЄМОДІЯ</span>   
+                    </a>
+                </div>
+            </section>
         </div>
         <footer>
             <div class="line"></div>
