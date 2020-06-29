03620a5de30c72f397cbc15c3f5bc904e6ab6cd5
Alexandr Stecuk <sandrostecuk@gmail.com>
2020-06-29 23:03:05 +0300

    update(*): style, layout

--- resources/views/blocks/layout.blade.php
+++ resources/views/blocks/layout.blade.php
@@ -6,9 +6,19 @@
     <title>Document</title>
     <link rel="stylesheet" href="/style/fonts.css">
     <link rel="stylesheet" href="/style.css">
-    <link rel="stylesheet" href="/style/home.css">
-    <link rel="stylesheet" href="/style/article.css">
-    <link rel="stylesheet" href="/style/model.css">
+
+    @if (Route::currentRouteName() === "home")
+       <link rel="stylesheet" href="/style/home.css">     
+    @endif
+
+    @if (Route::currentRouteName() === "model")
+       <link rel="stylesheet" href="/style/model.css">     
+    @endif
+
+    @if (Route::currentRouteName() === "model-article")
+       <link rel="stylesheet" href="/style/article.css">     
+    @endif
+       
     <link rel="stylesheet" href="/style/mediaquery.css">
     <link rel="stylesheet" href="/style/fontawesome/all.min.css">
 </head>
@@ -19,13 +29,14 @@
             <nav class="menu">
                 <a href="/" class="menu-item">Головна</a>
                 <a href="/model" class="menu-item">Модель розвитку</a>      
+                <a href="/english_model" class="menu-item">Поглиблене англомовлення</a>      
             </nav>
             <section class="slider">
                 <div class="hide-wrapper"></div>
-                <img id="sliderImage1" class="slider-image" src="/images/slider-img1.png" alt="kid">
-                <img id="sliderImage2" class="slider-image" src="/images/slider-img2.png" alt="kid">
-                <img id="sliderImage3" class="slider-image" src="/images/slider-img3.png" alt="kid">
-                <img id="sliderImage4" class="slider-image" src="/images/slider-img4.png" alt="kid">
+                <img id="sliderImage1" class="slider-image" src="/images/slider-img1.jpg" alt="kid">
+                <img id="sliderImage2" class="slider-image" src="/images/slider-img2.jpg" alt="kid">
+                <img id="sliderImage3" class="slider-image" src="/images/slider-img3.jpg" alt="kid">
+                <img id="sliderImage4" class="slider-image" src="/images/slider-img4.jpg" alt="kid">
                 <img src="/images/logo.png" alt="logo" class="logo">
             </section>
         </header>
@@ -35,10 +46,6 @@
         <footer>
             <section class="social-links">
                 <span class="follow">Follow us:</span>
-                <a href="https://facebook.com" class="social">
-                    <i class="fab fa-facebook-f"></i>
-                   Знайдіть нас в
-                </a>
 
                 <a href="https://www.facebook.com/Googliki/" class="social">
                     <i class="fab fa-facebook-f"></i>
